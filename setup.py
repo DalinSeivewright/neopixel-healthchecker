c2b1bf34f7b76400d414cfe751b8d6b1470bdb1e
Dalin Seivewright <dalin.seivewright@gmail.com>
2020-05-27 00:24:08 -0600

    Fixing dep version.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='Ping servers and display the result on a neopixel-like display',
     long_description=open('README.rst').read(),
     install_requires=[
-        "rpi_ws281x >= 4.3.2",
+        "rpi_ws281x >= 4.2.3",
         "adafruit-circuitpython-neopixel >= 6.0.0",
         "adafruit-blinka >= 4.9.0"
     ],
